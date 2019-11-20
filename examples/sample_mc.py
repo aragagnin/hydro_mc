43635d7df6a6d21ec33a84441570ad3473157604
aragagnin <57585343+aragagnin@users.noreply.github.com>
2019-11-20 15:51:40 +0100

    Update sample_mc.py

--- examples/sample_mc.py
+++ examples/sample_mc.py
@@ -1,10 +1,15 @@
 #
-# The following python script get's the 200c concetration of a halo with M_200c=1e14Msun halo at a=0.1,
+# Example for Ragagnin et al. (2020)
+#
+# To make this script work you need to import hydro_mc,
+# to do so, copy the hydro_mc.py file in the same folder of this file (or in your repository)
+#
+# The following python script get's the 200c concetration of a halo with M_200c=1e14Msun halo at a=0.9,
 # with a cosmology (omega_m = 0.2, omega_b = 0.04, sigma8=0.7, h0=0.7).
 #
 # The conversion uses the relation in Table 4 of Ragagnin et al. (2020).
 #
 
 import hydro_mc
-c_200 = hydro_mc.concentration_from_mc_relation('200c', M=1e14, a=0.1, omega_m = 0.2, omega_b = 0.04, sigma8=0.7, h0=0.7)
-print('c_200 = ',c_200)
+c_200 = hydro_mc.concentration_from_mc_relation('200c', M=1e14, a=0.9, omega_m = 0.2, omega_b = 0.04, sigma8=0.7, h0=0.7)
+print('c_200(M_200c=1e14Msun, a=0.9) = %.2f'%c_200)
