6c900b6d0204e5fcf026306e6fec04e1ce2f744c
aragagnin <57585343+aragagnin@users.noreply.github.com>
2019-11-20 16:09:26 +0100

    Update sample_mm.py

--- examples/sample_mm.py
+++ examples/sample_mm.py
@@ -14,11 +14,11 @@ print(' The conversion uses the relation in Table 5 and Table 6 of Ragagnin et a
 
 import hydro_mc
 M_200c = hydro_mc.mass_from_mm_relation('500c','200c', M=1e14, a=0.9,omega_m = 0.2, omega_b = 0.04, sigma8=0.7, h0=0.7)
-print('M_200c(M_500c=1e14Msun, a=0.9) = %e'%M_200c)
+print('M_200c(M_500c=1e14Msun, a=0.9) = %e Msun'%M_200c)
 
 print('The following python script obtains  M_200m given M_200c and the same properties as the previous example')
 
 import hydro_mc
 M_200m = hydro_mc.mass_from_mm_relation('200m','200c', M=1e14, a=0.9,omega_m = 0.2, omega_b = 0.04, sigma8=0.7, h0=0.7)
-print('M_200m(M_200c=1e14Msun, a=0.9) = %e '%M_200m)
+print('M_200m(M_200c=1e14Msun, a=0.9) = %e Msun'%M_200m)
 print('')
