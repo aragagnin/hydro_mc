104a1510105cb43dddebb06a39371cc140d12e14
aragagnin <57585343+aragagnin@users.noreply.github.com>
2019-11-20 15:53:10 +0100

    Update sample_mc_dm_lite.py

--- examples/sample_mc_dm_lite.py
+++ examples/sample_mc_dm_lite.py
@@ -1,4 +1,9 @@
 #
+# Example for Ragagnin et al. (2020)
+#
+# To make this script work you need to import hydro_mc,
+# to do so, copy the hydro_mc.py file in the same folder of this file (or in your repository)
+#
 # The following python script get's the 200c concetration of a   halo with M_200c=1e14Msun halo at a=0.1,
 # with a cosmology (omega_m = 0.2, omega_b = 0.04, sigma8=0.7, h0=0.7).
 #
@@ -6,9 +11,9 @@
 #
 
 import hydro_mc
-c_200 = hydro_mc.concentration_from_mc_relation('200c', M=1e14, a=0.1,
+c_200 = hydro_mc.concentration_from_mc_relation('200c', M=1e14, a=0.9,
               omega_m = 0.2, omega_b = 0.04, sigma8=0.7, h0=0.7,
               use_lite_mc_fit=True,
               use_lite_mc_dm_fit=True
               )
-print('c_200 = ',c_200)
+print('c_200c(M_200c=1e14Msun, a=0.9) = %.3f # from MC relation where mass log-slope does not depend on cosmology'%c_200)
