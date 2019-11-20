4542131edea2b1c2e4474b804236fcf441187276
aragagnin <57585343+aragagnin@users.noreply.github.com>
2019-11-20 15:55:09 +0100

    Update sample_mc_dm_lite.py

--- examples/sample_mc_dm_lite.py
+++ examples/sample_mc_dm_lite.py
@@ -4,11 +4,12 @@
 # To make this script work you need to import hydro_mc,
 # to do so, copy the hydro_mc.py file in the same folder of this file (or in your repository)
 #
-# The following python script get's the 200c concetration of a   halo with M_200c=1e14Msun halo at a=0.1,
-# with a cosmology (omega_m = 0.2, omega_b = 0.04, sigma8=0.7, h0=0.7).
-#
-# The conversion uses the dark matter NFW concentration in the mass-concentration relation presented in Table A2 of Ragagnin et al. (2020).
-#
+
+print('The following python script get's the 200c concetration of a   halo with M_200c=1e14Msun halo at a=0.1,')
+print('with a cosmology (omega_m = 0.2, omega_b = 0.04, sigma8=0.7, h0=0.7).')
+print('')
+print('The conversion uses the dark matter NFW concentration in the mass-concentration relation presented in Table A2 of Ragagnin et al. (2020).')
+print('')      
 
 import hydro_mc
 c_200 = hydro_mc.concentration_from_mc_relation('200c', M=1e14, a=0.9,
@@ -16,4 +17,5 @@ c_200 = hydro_mc.concentration_from_mc_relation('200c', M=1e14, a=0.9,
               use_lite_mc_fit=True,
               use_lite_mc_dm_fit=True
               )
-print('c_200c(M_200c=1e14Msun, a=0.9) = %.3f # from MC relation where mass log-slope does not depend on cosmology'%c_200)
+print('c_200c(M_200c=1e14Msun, a=0.9) = %.3f  '%c_200)
+print('')
