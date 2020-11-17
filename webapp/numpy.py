d511fcfd06d020e583e8ff6006e2442261ffda56
Your Name <you@example.com>
2020-11-17 19:05:51 +0100

    web app mass and concentration calculator

---
+++ webapp/numpy.py
@@ -0,0 +1,17 @@
+#
+# not the best numpy implementation 
+#
+
+from math import *
+
+def abs(x):
+    if x>0:
+        return x
+    else:
+        return -x
+def all(x):
+    return x
+
+
+def array(a):
+    return a
